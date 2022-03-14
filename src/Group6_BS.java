aa39b7b7dfa7d665f08b834b2dbd6e01c51baa61
Renzo <renzoschram@gmail.com>
2022-03-14 15:33:13 +0100

    added static variables and setup for few stages, make it dynamic later

--- src/Group6_BS.java
+++ src/Group6_BS.java
@@ -1,6 +1,8 @@
 import genius.core.Bid;
 import genius.core.bidding.BidDetails;
 import genius.core.boaframework.*;
+import genius.core.utility.AbstractUtilitySpace;
+
 import java.util.concurrent.TimeUnit;
 
 import java.util.ArrayList;
@@ -9,15 +11,30 @@ import java.util.Map;
 public class Group6_BS extends OfferingStrategy {
     private SortedOutcomeSpace possibleAgentBids;
     private BidDetails startingBid;
+    private double tresholdUtility;
+    private double startingBidUtility;
+    private double stageOneAllowedTime;
+    private double stageTwoAllowedTime;
+    private double stageThreeAllowedTime;
+    private double scareTacticUtility;
 
     @Override
     public void init(NegotiationSession negotiationSession, OpponentModel opponentModel, OMStrategy omStrategy,
                      Map<String, Double> parameters) throws Exception {
-        super.init(negotiationSession, parameters);
+        super.init(negotiationSession, opponentModel, omStrategy, parameters);
         possibleAgentBids = new SortedOutcomeSpace(negotiationSession.getUtilitySpace());
         negotiationSession.setOutcomeSpace(possibleAgentBids);
 
-        startingBid = possibleAgentBids.getBidNearUtility(0.8);
+        startingBidUtility = 0.8;
+        //TODO make tresholdUtility value dynamical in between stages
+        tresholdUtility = 0.8;
+        stageOneAllowedTime = 0.2;
+        stageTwoAllowedTime = 0.8;
+        stageThreeAllowedTime = 0.85;
+        scareTacticUtility = 0.9;
+
+
+        startingBid = possibleAgentBids.getBidNearUtility(startingBidUtility);
     }
 
     @Override
@@ -30,17 +47,24 @@ public class Group6_BS extends OfferingStrategy {
         double timePassed = negotiationSession.getTimeline().getTime();
         System.out.println(timePassed);
 
-        if (timePassed < 0.2){
+        if (timePassed < stageOneAllowedTime /* TODO add here when opponentmodel is accurate enough, discuss with Rick and Marije */) {
             // stage 1 hardheaded bid while determining opponent model + strategy
             return startingBid;
-        } else if (timePassed < 0.5){
+        } else if (timePassed < stageTwoAllowedTime) {
             // stage 2 Perlin-noise tactic while conceding based on opponent conceding factor
-            return null;
-        } else if (timePassed < 0.8) {
+            if (true /*TODO add method for determining if opponent model is reliable and return bool*/) {
+                /*TODO discuss how we get concedingfactor of opponent (from opponent model strategy?)*/
+            } else {
+                /*TODO determine what to do if there is no (reliable) opponent model*/
+            }
+            return startingBid;
+        } else if (timePassed < stageThreeAllowedTime) {
             // stage 3
-            return null;
+            /*TODO possibly make scareTacticUtility a dynamic value*/
+            return possibleAgentBids.getBidNearUtility(scareTacticUtility);
         } else {
             // stage 4
+            //BidDetails bid = omStrategy.getBid(opponentModel.getOpponentUtilitySpace(), tresholdUtility);
             return null;
         }
     }
